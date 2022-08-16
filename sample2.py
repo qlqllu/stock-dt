54c84ca0cdc4c0fd16e07a0998d06a55471f6548
Junshan Liu <junshan_liu@esri.com>
2022-08-16 14:49:33 +0800

    sample 2

--- sample2.py
+++ sample2.py
@@ -1,10 +1,10 @@
 from sklearn import tree
 from matplotlib import pyplot as plt
 
-X = [[0, 0], [1, 1], [2, 2], [3, 3], [4, 4]]
-y = [0.5, 2.5, 3.5, 4.5, 5.5]
-model = tree.DecisionTreeRegressor()
-model = model.fit(X, y)
+X = [[0, 0], [1, 1]]
+Y = [0, 1]
+model = tree.DecisionTreeClassifier()
+model = model.fit(X, Y)
 print(model.predict([[1, 10]]))
 tree.plot_tree(model, feature_names=['f1', 'f2'], fontsize=10)
 plt.show()
