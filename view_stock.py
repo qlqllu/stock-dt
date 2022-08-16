efed9481886c8ad49789c42f8f4a63d47faac8a2
Junshan Liu <junshan_liu@esri.com>
2022-08-16 16:27:15 +0800

    view stock

--- view_stock.py
+++ view_stock.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import mplfinance as mpf
 
-stock_file = 'E:\\github\\C3-Data-Science\\backtest\\datas\\stock\\zh_a\\sh600571.csv'
+stock_file = 'E:\\github\\C3-Data-Science\\backtest\\datas\\stock\\zh_a\\sz002005.csv'
 
 daily = pd.read_csv(stock_file, index_col=0, parse_dates=True)
+daily = daily.loc[:, ['open', 'close', 'high', 'low', 'volume']]
+
 daily['color'] = daily.apply(lambda x: 'red' if (x.close - x.open) > 0 else 'green', axis=1)
-# print(daily.head(3)['color'].values)
-plotData = daily.tail(100)
-mpf.plot(plotData, type='candle', marketcolor_overrides=plotData['color'].values)
+daily['price_change'] = daily.close - daily.close.shift(1)
+daily['price_change_pct'] = round((daily.close - daily.close.shift(1)) / daily.close.shift(1) * 100, 2)
+print(daily.tail(5))
+# plotData = daily.tail(100)
+# mpf.plot(plotData, type='candle', marketcolor_overrides=plotData['color'].values)
